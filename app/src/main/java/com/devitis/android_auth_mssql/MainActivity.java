e4c12ef118bc5149994682e136e549eff839f544
Diana <diana.sinenchenko@gmail.com>
2019-01-31 10:29:26 +0200

    initialization layout elements

--- app/src/main/java/com/devitis/android_auth_mssql/MainActivity.java
+++ app/src/main/java/com/devitis/android_auth_mssql/MainActivity.java
@@ -2,12 +2,28 @@ package com.devitis.android_auth_mssql;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button login_button;
+    EditText user_name_editText,password_editText;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login_button = (Button) findViewById(R.id.login_button);
+        user_name_editText = (EditText) findViewById(R.id.user_name_editText);
+        password_editText = (EditText) findViewById(R.id.password_editText);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.GONE);
+
+
     }
 }
